56ce3004329406709a7bc1b1279e43dac13c750a
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-08-26 11:41:18 +0800

    0826

--- root/usr/share/chinadns-ng/chinadns-ng_update.sh
+++ root/usr/share/chinadns-ng/chinadns-ng_update.sh
@@ -2,13 +2,13 @@
 set -o pipefail
 
 [ -d /etc/chinadns-ng ] || mkdir /etc/chinadns-ng
-wget https://pexcn.me/daily/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
+wget https://cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
 mv -f /tmp/chnroute.txt.tmp /etc/chinadns-ng/chnroute.txt
-wget https://pexcn.me/daily/chnroute/chnroute-v6.txt -O /tmp/chnroute-v6.txt.tmp
+wget https://cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute-v6.txt -O /tmp/chnroute-v6.txt.tmp
 mv -f /tmp/chnroute-v6.txt.tmp /etc/chinadns-ng/chnroute6.txt
-wget https://pexcn.me/daily/gfwlist/gfwlist.txt -O /tmp/gfwlist.txt.tmp
+wget https://cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/gfwlist.txt -O /tmp/gfwlist.txt.tmp
 mv -f /tmp/gfwlist.txt.tmp /etc/chinadns-ng/gfwlist.txt
-wget https://pexcn.me/daily/chinalist/chinalist.txt -O /tmp/chinalist.txt.tmp
+wget https://cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chinalist.txt -O /tmp/chinalist.txt.tmp
 mv -f /tmp/chinalist.txt.tmp /etc/chinadns-ng/chinalist.txt
 
 /etc/init.d/chinadns-ng restart
